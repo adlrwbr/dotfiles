98c744593f8c31632b3e1ab75e7bd180bc0cf46f
Adler Weber <amw369@cornell.edu>
2021-12-22 20:54:08 -0500

    Add autocompletion

--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -16,6 +16,15 @@ lsp_installer.on_server_ready(function(server)
     end
     server:setup(opts)
 end)
+lsp_installer.settings({
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
+})
 -- Key Mappings
 -- See `:help vim.lsp.*` for documentation on any of the below functions
 local function nkeymap(key, map)
@@ -25,12 +34,12 @@ nkeymap('gD', ':lua vim.lsp.buf.declaration()<CR>')
 nkeymap('gd', ':lua vim.lsp.buf.definition()<CR>')
 nkeymap('gi', ':lua vim.lsp.buf.implementation()<CR>')
 nkeymap('gr', ':lua vim.lsp.buf.references()<CR>')
+nkeymap('gT', ':lua vim.lsp.buf.type_definition()<CR>')
 nkeymap('K', ':lua vim.lsp.buf.hover()<CR>')
 nkeymap('<C-k>', ':lua vim.lsp.buf.signature_help()<CR>')
 nkeymap('<leader>wa', ':lua vim.lsp.buf.add_workspace_folder()<CR>')
 nkeymap('<leader>wr', ':lua vim.lsp.buf.remove_workspace_folder()<CR>')
 nkeymap('<leader>wl', ':lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
-nkeymap('<leader>D', ':lua vim.lsp.buf.type_definition()<CR>')
 nkeymap('<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
 nkeymap('<leader>ca', ':lua vim.lsp.buf.code_action()<CR>')
 nkeymap('<leader>e', ':lua vim.diagnostic.open_float()<CR>')
@@ -38,3 +47,76 @@ nkeymap('[d', ':lua vim.diagnostic.goto_prev()<CR>')
 nkeymap(']d', ':lua vim.diagnostic.goto_next()<CR>')
 nkeymap('<leader>q', ':lua vim.diagnostic.setloclist()<CR>')
 nkeymap('<leader>f', ':lua vim.lsp.buf.formatting()<CR>')
+
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+cmp.setup({
+    mapping = {
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        -- Like JetBrains: Tab to replace, Enter to insert
+        -- TODO: Insert not working. See https://github.com/hrsh7th/nvim-cmp/issues/664
+        -- Accept currently selected item. If none selected, `select` first item.
+        -- Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+        ['<Tab>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    },
+    -- Priority of sources:
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'path' },
+        { name = 'emoji' },
+        { name = 'buffer', keyword_length = 4 },
+    }),
+    formatting = {
+        format = require("lspkind").cmp_format({
+            with_text = true,
+            menu = ({
+                buffer = '[Buf]',
+                path = '[Path]',
+                nvim_lsp = '[LSP]',
+                nvim_lua = '[API]',
+                emoji = '[Emoji]',
+                -- latex_symbols = '[Latex]', -- TODO install
+            })
+        }),
+    },
+    experimental = {
+        ghost_text = true,
+        native_menu = false
+    }
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+    sources = cmp.config.sources({
+        { name = 'buffer' },
+        { name = 'emoji' }
+    })
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+        { name = 'path' },
+        { name = 'cmdline' }
+    })
+})
+
+-- Advertise nvim-cmp support to LSP (enables snippets and whatnot)
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local servers = lsp_installer.get_installed_servers()
+for _, server in ipairs(servers) do
+    local server_name = server['name']
+    require('lspconfig')[server_name].setup {
+        -- cmd = {'/home/adler/.local/share/nvim/lsp_servers/sumneko_lua/extension/server/bin/lua-language-server'},
+        capabilities = capabilities
+    }
+end
--- nvim/plugins/lsp.vim
+++ nvim/plugins/lsp.vim
@@ -1,5 +1,16 @@
+" LSP 
 Plug 'neovim/nvim-lspconfig'
 Plug 'williamboman/nvim-lsp-installer'
+
+" Autocompletion
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'onsails/lspkind-nvim'
+" Autocompletion sources
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-emoji'
 
 augroup LSPSetup
     autocmd!
